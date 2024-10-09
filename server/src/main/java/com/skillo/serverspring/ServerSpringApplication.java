475a441e1493ba6d89b01a32af2229521be478bf
mmarci96 <sarosdimarci@gmail.com>
2024-10-09 07:09:12 +0000

    server init

---
+++ server/src/main/java/com/skillo/serverspring/ServerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.skillo.serverspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerSpringApplication.class, args);
+    }
+
+}
