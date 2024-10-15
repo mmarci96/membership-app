310e627cd8ad055164d0e4f76a9974fcb6bd8f0c
mmarci96 <sarosdimarci@gmail.com>
2024-10-15 15:46:28 +0000

    starter code

---
+++ server/src/main/java/com/codecool/sv_server/SvServerApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.sv_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/codecool/sv_server/SvServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.sv_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
