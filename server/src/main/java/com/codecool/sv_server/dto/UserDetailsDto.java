edf7e2d88f2e94ada2bd3bae81251bd0f2b8da4e
mmarci96 <sarosdimarci@gmail.com>
2024-10-18 00:08:15 +0200

    Setting up userdetails service

---
+++ server/src/main/java/com/codecool/sv_server/dto/UserDetailsDto.java
@@ -0,0 +1,6 @@
+package com.codecool.sv_server.dto;
+
+public record UserDetailsDto(String firstName, String lastName,
+                             String phoneNumber, String address, String city,
+                             String country, long id) {
+}
