7343a360e71de952c84057d3da88b99789c66ca6
mmarci96 <sarosdimarci@gmail.com>
2024-10-22 20:40:01 +0200

    created blogposts for content

---
+++ server/src/main/java/com/codecool/sv_server/controller/BlogPostController.java
@@ -0,0 +1,33 @@
+package com.codecool.sv_server.controller;
+
+import com.codecool.sv_server.dto.BlogPostDto;
+import com.codecool.sv_server.service.BlogPostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/blog")
+public class BlogPostController {
+
+    private final BlogPostService blogPostService;
+
+    @Autowired
+    public BlogPostController(BlogPostService blogPostService) {
+        this.blogPostService = blogPostService;
+    }
+
+    @GetMapping("/posts")
+    public List<BlogPostDto> getPosts() {
+        return blogPostService.getAllBlogPosts().toList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BlogPostDto> getPostById(@PathVariable String id) {
+        Long blogPostId = Long.parseLong(id);
+        BlogPostDto blogPostDto = blogPostService.getBlogPostById(blogPostId);
+        return ResponseEntity.ok(blogPostDto);
+    }
+}
