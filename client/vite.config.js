e6e890e2db6b40dade3d7c86b06107e49ff252f0
mmarci96 <sarosdimarci@gmail.com>
2024-10-22 20:15:14 +0200

    preparing frontend, for testing lets create some blogpost

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // URL of your backend server
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '') // Optionally, remove '/api' prefix from request
+      }
+    }
+  }
 })
