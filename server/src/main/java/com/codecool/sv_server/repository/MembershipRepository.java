1caf59dca949c0137d8af3cebe1f5c97ebea5ff5
mmarci96 <sarosdimarci@gmail.com>
2024-10-24 20:39:15 +0200

    bugs fixed

--- server/src/main/java/com/codecool/sv_server/repository/MembershipRepository.java
+++ server/src/main/java/com/codecool/sv_server/repository/MembershipRepository.java
@@ -3,8 +3,10 @@ package com.codecool.sv_server.repository;
 import com.codecool.sv_server.entity.Membership;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
-    @Query("SELECT m FROM Membership m WHERE m.user.email = :email AND m.subscriptionStatus = 'ACTIVE'")
-    Membership findActiveByUserEmail(String email);
+    @Query("SELECT m FROM Membership m JOIN m.user u WHERE u.email = :email AND m.subscriptionStatus = com.codecool.sv_server.entity.SubscriptionStatus.ACTIVE")
+    Membership findActiveByUserEmail(@Param("email") String email);
+
 }
