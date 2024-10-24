008abc429ac16a6705b6cdad6268f35f2db6a02c
mmarci96 <sarosdimarci@gmail.com>
2024-10-24 19:58:16 +0200

    added all aspects for authorizeing and giving membership context to user

---
+++ server/src/main/java/com/codecool/sv_server/repository/MembershipModuleRepository.java
@@ -0,0 +1,10 @@
+package com.codecool.sv_server.repository;
+
+import com.codecool.sv_server.entity.MembershipModule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MembershipModuleRepository extends JpaRepository<MembershipModule, Long> {
+    List<MembershipModule> findByMembershipPackageId(Long packageId);
+}
