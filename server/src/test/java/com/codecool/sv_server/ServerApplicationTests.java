aa4465333f9452d0a5d085de98be5236190a9368
mmarci96 <sarosdimarci@gmail.com>
2025-04-22 12:02:59 +0200

    at this point the docker compose worked properly but the builds are very much not optimal

--- server/src/test/java/com/codecool/sv_server/ServerApplicationTests.java
+++ server/src/test/java/com/codecool/sv_server/ServerApplicationTests.java
@@ -1,13 +1,25 @@
 package com.codecool.sv_server;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.codecool.sv_server.service.HelloService;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ServerApplicationTests {
+    @Autowired
+    private HelloService helloService;
 
-	@Test
-	void contextLoads() {
-	}
+    // @Test
+    // void contextLoads() {
+    // }
+
+    @Test
+    void helloServiceReturnsCorrectMessage() {
+        String message = helloService.sayHello();
+        assertThat(message).isEqualTo("Hello, World!");
+    }
 
 }
