5524b830a74eccccaf744a6ba1d6dd9384656077
mmarci96 <sarosdimarci@gmail.com>
2025-04-22 14:20:49 +0200

    not sure if its a great idea to load the env like this. but might only be needed on tests.

--- server/src/main/java/com/codecool/sv_server/ServerApplication.java
+++ server/src/main/java/com/codecool/sv_server/ServerApplication.java
@@ -1,6 +1,9 @@
 package com.codecool.sv_server;
 
 import com.codecool.sv_server.config.RsaKeyProperties;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -9,8 +12,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @EnableConfigurationProperties(RsaKeyProperties.class)
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        SpringApplication.run(ServerApplication.class, args);
+    }
 
 }
