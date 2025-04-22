d6661370b81bdff0fab057320238332fa72dcf37
mmarci96 <sarosdimarci@gmail.com>
2025-04-22 12:34:57 +0200

    removed comments

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		headers: {
-			'Content-Security-Policy':
-				"default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';",
-		},
-		proxy: {
-			'/api': {
-				target: 'http://backend:8080', // URL of your backend server
-				changeOrigin: true,
-			},
-		},
-	},
-	build: {
-		// Production build configurations
-		outDir: 'dist',
-	},
+    plugins: [react()],
+    server: {
+        headers: {
+            "Content-Security-Policy":
+                "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';",
+        },
+        proxy: {
+            "/api": {
+                target: "http://backend:8080",
+                changeOrigin: true,
+            },
+        },
+    },
+    build: {
+        outDir: "dist",
+    },
 });
