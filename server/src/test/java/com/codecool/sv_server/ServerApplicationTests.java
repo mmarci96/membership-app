52b95aecd8d30716f60563428f15684dfdd3c402
mmarci96 <sarosdimarci@gmail.com>
2025-04-22 14:25:47 +0200

    test starts and hello world service seems to be working on the environment

--- server/src/test/java/com/codecool/sv_server/ServerApplicationTests.java
+++ server/src/test/java/com/codecool/sv_server/ServerApplicationTests.java
@@ -1,10 +1,17 @@
 package com.codecool.sv_server;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import com.codecool.sv_server.service.HelloService;
 
 @SpringBootTest
 class ServerApplicationTests {
+    @Autowired
+    private HelloService helloService;
 
     static {
         io.github.cdimascio.dotenv.Dotenv dotenv = io.github.cdimascio.dotenv.Dotenv.load();
@@ -13,6 +20,12 @@ class ServerApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void helloServiceReturnsCorrectMessage() {
+        String message = helloService.sayHello();
+        assertThat(message).isEqualTo("Hello, World!");
     }
 
 }
