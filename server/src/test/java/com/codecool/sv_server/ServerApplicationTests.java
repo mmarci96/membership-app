128b87ddaffdbbd0dee613fc6e36bf42cee708ff
mmarci96 <sarosdimarci@gmail.com>
2025-04-23 15:53:00 +0200

    some very simple tests are implemented just to get the hang of it, they print a lot of context that i dont care about for now

--- server/src/test/java/com/codecool/sv_server/ServerApplicationTests.java
+++ server/src/test/java/com/codecool/sv_server/ServerApplicationTests.java
@@ -1,29 +1,13 @@
 package com.codecool.sv_server;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import com.codecool.sv_server.service.HelloService;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ServerApplicationTests {
-    @Autowired
-    private HelloService helloService;
-
-    static {
-        io.github.cdimascio.dotenv.Dotenv dotenv = io.github.cdimascio.dotenv.Dotenv.load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-    }
-
     @Test
     void contextLoads() {
     }
-
-    @Test
-    void helloServiceReturnsCorrectMessage() {
-        String message = helloService.sayHello();
-        assertThat(message).isEqualTo("Hello, World!");
-    }
-
 }
