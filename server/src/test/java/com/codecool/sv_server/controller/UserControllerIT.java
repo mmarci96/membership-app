1b72026403a4c46404cebfba51e7bbb3e776c861
mmarci96 <sarosdimarci@gmail.com>
2025-04-29 09:21:30 +0200

    test for unhappy cases added to cover the most expected behaviors or possible issues

--- server/src/test/java/com/codecool/sv_server/controller/UserControllerIT.java
+++ server/src/test/java/com/codecool/sv_server/controller/UserControllerIT.java
@@ -1,6 +1,8 @@
 package com.codecool.sv_server.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -62,6 +64,126 @@ public class UserControllerIT extends BaseIntegrationTest {
                          .contentType(MediaType.APPLICATION_JSON))
             .andExpect(content().json(response.toString()))
             .andExpect(status().isOk());
+    }
+
+    @Test
+    public void test_updating_user_details_with_valid_data() throws Exception {
+        var email = "update_test@happy.test";
+        postValidUserDetails(email);
+
+        JsonNode loginRes = loginUser(email, "Password1");
+        String token = loginRes.get("token").asText();
+        long userId = loginRes.get("userId").asLong();
+
+        var updatedDetails = new UserDetailsDto(
+            "UpdatedFirst", "UpdatedLast", "+9876543210", "456 Updated Street",
+            "UpdatedCity", "UC", userId, "67890");
+        var updatedJson = objectMapper.writeValueAsString(updatedDetails);
+
+        mockMvc
+            .perform(patch("/api/users/account")
+                         .header("Authorization", "Bearer " + token)
+                         .contentType(MediaType.APPLICATION_JSON)
+                         .content(updatedJson))
+            .andExpect(status().isOk())
+            .andExpect(content().json(updatedJson));
+    }
+
+    @Test
+    public void test_deleting_user_details_with_valid_data() throws Exception {
+        var email = "delete_test@happy.test";
+        postValidUserDetails(email);
+
+        JsonNode loginRes = loginUser(email, "Password1");
+        String token = loginRes.get("token").asText();
+        long userId = loginRes.get("userId").asLong();
+
+        var userDetailsDto = new UserDetailsDto(
+            "First", "Last", "+1111111111", "111 Delete Street", "DeleteCity",
+            "DC", userId, "11111");
+        var userDetailsJson = objectMapper.writeValueAsString(userDetailsDto);
+
+        mockMvc
+            .perform(delete("/api/users/account/" + userId)
+                         .header("Authorization", "Bearer " + token)
+                         .contentType(MediaType.APPLICATION_JSON)
+                         .content(userDetailsJson))
+            .andExpect(status().isOk())
+            .andExpect(content().string("User details deleted succesfully!"));
+    }
+
+    @Test
+    public void test_updating_user_details_without_auth_should_fail()
+        throws Exception {
+        var updatedDetails = new UserDetailsDto("First", "Last", "+1111111111",
+                                                "111 Test Street", "City",
+                                                "Country", 1L, "11111");
+        var updatedJson = objectMapper.writeValueAsString(updatedDetails);
+
+        mockMvc
+            .perform(patch("/api/users/account")
+                         .contentType(MediaType.APPLICATION_JSON)
+                         .content(updatedJson))
+            .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void test_updating_other_users_details_should_fail()
+        throws Exception {
+        var email = "unauthorized_update@fail.test";
+        postValidUserDetails(email);
+        JsonNode loginRes = loginUser(email, "Password1");
+        String token = loginRes.get("token").asText();
+
+        var anotherUserId = 9999L;
+        var updatedDetails =
+            new UserDetailsDto("First", "Last", "+1111111111", "Fake Street",
+                               "City", "Country", anotherUserId, "11111");
+        var updatedJson = objectMapper.writeValueAsString(updatedDetails);
+
+        mockMvc
+            .perform(patch("/api/users/account")
+                         .header("Authorization", "Bearer " + token)
+                         .contentType(MediaType.APPLICATION_JSON)
+                         .content(updatedJson))
+            .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void test_deleting_user_details_without_auth_should_fail()
+        throws Exception {
+        var userDetailsDto =
+            new UserDetailsDto("First", "Last", "+1111111111", "Some Street",
+                               "City", "Country", 1L, "12345");
+        var userDetailsJson = objectMapper.writeValueAsString(userDetailsDto);
+
+        mockMvc
+            .perform(delete("/api/users/account/1")
+                         .contentType(MediaType.APPLICATION_JSON)
+                         .content(userDetailsJson))
+            .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void test_deleting_other_users_details_should_fail()
+        throws Exception {
+        var email = "unauthorized_delete@fail.test";
+        postValidUserDetails(email);
+        JsonNode loginRes = loginUser(email, "Password1");
+        String token = loginRes.get("token").asText();
+
+        var anotherUserId = 9999L;
+        var userDetailsDto = new UserDetailsDto(
+            "First", "Last", "+1111111111", "Fake Delete Street", "City",
+            "Country", anotherUserId, "12345");
+        var userDetailsJson = objectMapper.writeValueAsString(userDetailsDto);
+
+        mockMvc
+            .perform(delete("/api/users/account/" + anotherUserId)
+                         .header("Authorization", "Bearer " + token)
+                         .contentType(MediaType.APPLICATION_JSON)
+                         .content(userDetailsJson))
+            .andExpect(status().isForbidden());
     }
 
     private JsonNode postValidUserDetails(String email) throws Exception {
